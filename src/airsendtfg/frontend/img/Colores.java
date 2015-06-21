48f3847945e77437a3b9da2c3849e1c399b31aa1
Pablo <Pablo@192.168.1.36>
2015-06-21 20:46:25 +0200

    Empezando...

---
+++ src/airsendtfg/frontend/img/Colores.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airsendtfg.frontend.img;
+
+import java.awt.Color;
+
+
+/**
+ *
+ * @author Pablo
+ */
+public class Colores {
+    
+    /**
+     * Color de fondo de la aplicación
+     * @return 
+     */
+    public static Color fondo(){
+        Color fondo =new Color(18, 23, 28);
+        return fondo;
+    }
+    
+}
